42b0c63b21368c006f018de79b6412f7a877a193
EvgenEng <zhenya-chaplygin@yandex.ru>
2025-09-26 22:13:08 +0300

    fix

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/GrpcEventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/GrpcEventController.java
@@ -9,6 +9,7 @@ import net.devh.boot.grpc.server.service.GrpcService;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.grpc.telemetry.service.CollectorControllerGrpc;
+import ru.yandex.practicum.model.hub.HubEvent;
 
 import java.util.Map;
 import java.util.Set;
@@ -20,13 +21,15 @@ import java.util.stream.Collectors;
 public class GrpcEventController extends CollectorControllerGrpc.CollectorControllerImplBase {
 
     private final Map<SensorEventProto.PayloadCase, SensorEventHandler> sensorEventHandlers;
+    private final EventService eventService;
 
-    public GrpcEventController(Set<SensorEventHandler> sensorEventHandlers) {
+    public GrpcEventController(Set<SensorEventHandler> sensorEventHandlers, EventService eventService) {
         this.sensorEventHandlers = sensorEventHandlers.stream()
                 .collect(Collectors.toMap(
                         SensorEventHandler::getMessageType,
                         Function.identity()
                 ));
+        this.eventService = eventService;
     }
 
     @Override
@@ -56,6 +59,9 @@ public class GrpcEventController extends CollectorControllerGrpc.CollectorContro
         try {
             log.info("Received hub event from hub: {}", request.getHubId());
 
+            HubEvent hubEvent = convertToHubEvent(request);
+            eventService.collectHubEvent(hubEvent);
+
             responseObserver.onNext(Empty.getDefaultInstance());
             responseObserver.onCompleted();
 
@@ -65,5 +71,57 @@ public class GrpcEventController extends CollectorControllerGrpc.CollectorContro
                     Status.INTERNAL.withDescription(e.getLocalizedMessage()).withCause(e)
             ));
         }
+    }
+
+    private HubEvent convertToHubEvent(HubEventProto proto) {
+        HubEventProto.PayloadCase payloadCase = proto.getPayloadCase();
+
+        switch (payloadCase) {
+            case DEVICE_ADDED:
+                var deviceAdded = proto.getDeviceAdded();
+                var deviceAddedEvent = new ru.yandex.practicum.model.hub.DeviceAddedEvent();
+                setCommonHubFields(deviceAddedEvent, proto);
+                deviceAddedEvent.setId(deviceAdded.getId());
+                deviceAddedEvent.setDeviceType(convertDeviceType(deviceAdded.getType()));
+                return deviceAddedEvent;
+
+            case DEVICE_REMOVED:
+                var deviceRemoved = proto.getDeviceRemoved();
+                var deviceRemovedEvent = new ru.yandex.practicum.model.hub.DeviceRemovedEvent();
+                setCommonHubFields(deviceRemovedEvent, proto);
+                deviceRemovedEvent.setId(deviceRemoved.getId());
+                return deviceRemovedEvent;
+
+            case SCENARIO_ADDED:
+                var scenarioAdded = proto.getScenarioAdded();
+                var scenarioAddedEvent = new ru.yandex.practicum.model.hub.ScenarioAddedEvent();
+                setCommonHubFields(scenarioAddedEvent, proto);
+                scenarioAddedEvent.setName(scenarioAdded.getName());
+                // TODO: добавить конвертацию условий и действий когда понадобится
+                return scenarioAddedEvent;
+
+            case SCENARIO_REMOVED:
+                var scenarioRemoved = proto.getScenarioRemoved();
+                var scenarioRemovedEvent = new ru.yandex.practicum.model.hub.ScenarioRemovedEvent();
+                setCommonHubFields(scenarioRemovedEvent, proto);
+                scenarioRemovedEvent.setName(scenarioRemoved.getName());
+                return scenarioRemovedEvent;
+
+            default:
+                throw new IllegalArgumentException("Unknown hub event type: " + payloadCase);
+        }
+    }
+
+    private void setCommonHubFields(HubEvent event, HubEventProto proto) {
+        event.setHubId(proto.getHubId());
+        event.setTimestamp(java.time.Instant.ofEpochSecond(
+                proto.getTimestamp().getSeconds(),
+                proto.getTimestamp().getNanos()
+        ));
+    }
+
+    private ru.yandex.practicum.model.hub.DeviceType convertDeviceType(
+            ru.yandex.practicum.grpc.telemetry.event.DeviceTypeProto protoType) {
+        return ru.yandex.practicum.model.hub.DeviceType.valueOf(protoType.name());
     }
 }
