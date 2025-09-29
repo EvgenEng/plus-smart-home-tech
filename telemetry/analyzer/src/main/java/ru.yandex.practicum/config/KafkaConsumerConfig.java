d001b149540dab2e4530aca7d0d30b2c55a00522
EvgenEng <zhenya-chaplygin@yandex.ru>
2025-09-29 21:43:42 +0300

    fix: fix

--- telemetry/analyzer/src/main/java/ru.yandex.practicum/config/KafkaConsumerConfig.java
+++ telemetry/analyzer/src/main/java/ru.yandex.practicum/config/KafkaConsumerConfig.java
@@ -1,25 +1,51 @@
 package ru.yandex.practicum.config;
 
-import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
-@RequiredArgsConstructor
+import java.util.Properties;
+
 @Configuration
 public class KafkaConsumerConfig {
-    private final KafkaConsumerProperties properties;
+
+    @Value("${kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
 
     @Bean
-    public Consumer<String, SensorsSnapshotAvro> getConsumerSensor() {
-        return new KafkaConsumer<>(properties.getSensorSnapshot());
+    public KafkaConsumer<String, SensorsSnapshotAvro> sensorSnapshotConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-snapshot-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put("schema.registry.url", "http://localhost:8081");
+        props.put("specific.avro.reader", "true");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+
+        return new KafkaConsumer<>(props);
     }
 
     @Bean
-    public Consumer<String, HubEventAvro> getConsumerHub() {
-        return new KafkaConsumer<>(properties.getHubEvent());
+    public KafkaConsumer<String, HubEventAvro> hubEventConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-hub-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put("schema.registry.url", "http://localhost:8081");
+        props.put("specific.avro.reader", "true");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "50");
+
+        return new KafkaConsumer<>(props);
     }
 }
