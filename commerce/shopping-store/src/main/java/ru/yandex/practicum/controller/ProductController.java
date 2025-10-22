21d8d76f073ee5d0c0dd9c9a1fe8b1bdd58d9fce
EvgenEng <zhenya-chaplygin@yandex.ru>
2025-10-22 15:37:12 +0300

    fix: add sorting parameter to getProducts endpoint

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ProductController.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ProductController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.dto.*;
+import ru.yandex.practicum.dto.ProductCategory;
+import ru.yandex.practicum.dto.ProductDto;
+import ru.yandex.practicum.dto.ProductNotFoundException;
+import ru.yandex.practicum.dto.QuantityState;
 import ru.yandex.practicum.mapper.ProductMapper;
 import ru.yandex.practicum.model.Product;
 import ru.yandex.practicum.service.ProductService;
@@ -34,8 +38,16 @@ public class ProductController {
     public Page<ProductDto> getProducts(
             @RequestParam ProductCategory category,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "productName,asc") String sort) {
+
+        String[] sortParams = sort.split(",");
+        String sortField = sortParams[0];
+        Sort.Direction direction = sortParams.length > 1 ?
+                Sort.Direction.fromString(sortParams[1]) : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
+
         return productService.getProductsByCategory(category, pageable)
                 .map(productMapper::toDto);
     }
